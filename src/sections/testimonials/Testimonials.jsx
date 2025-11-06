c38cb1bf01a5eb9abd4143703e5ed20573223f50
Angel Bustamante <bustamanteangel532@gmail.com>
2025-11-06 10:27:58 -0600

    feat: desarrollo de las secciones Testimonials y FAQs con funcionalidades interactivas y diseño responsive
    - Creación del componente Testimonials con estructura JSX y SwiperJS para el carrusel de testimonios
    - Implementación de los componentes Testimonial y configuración de breakpoints responsivos
    - Corrección de compatibilidad para Firefox y ajustes visuales con CSS
    - Adición de autoplay al carrusel de testimonios para una experiencia dinámica
    - Creación de la sección FAQs con componentes JSX modulares
    - Aplicación de estilos CSS y funcionalidad de toggle mediante useState
    - Validación del comportamiento responsive y coherencia visual con el resto del sitio

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,8 +1,52 @@
-import './testimonials.css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+// import './styles.css';
+
+// import required modules
+import { Pagination, Autoplay  } from 'swiper/modules';
+
+import testimonials from './data';
+import Testimonial from "./Testimonial";
+import './testimonials.css';
+
 
 const Testimonials = () => {
   return (
-    <section id="testimonials"><h2>Testimonials</h2></section>
+    <section id="testimonials">
+      <h2>Lo que dicen mis clientes</h2>
+      <p>
+        Estos son testimonios imparciales de algunos de mis clientes.
+      </p>
+      <div className="container">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+            601: {slidesPerView: 2},
+            1025: {slidesPerView: 3}
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        autoplay= {true}
+        className="mySwiper"
+      >
+        {
+          testimonials.map(testimonial => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial}/>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+      </div>
+    </section>
   )
 }
 
